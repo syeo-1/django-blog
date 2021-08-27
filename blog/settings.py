90d317cd7f867043227ef9b9ec2987057e9c3512
Sean Yeo <seansqyeo@gmail.com>
2021-08-27 16:45:59 -0700

    change imports

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8d+7@ly00_e+9*e3sxzy#glcz#g^bg1p@9#f84e^4=n6=b_1*8'
+SECRET_KEY = config.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
