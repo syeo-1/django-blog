e6d6198d5589e7e1231f8d42fcbeb2218cb61af2
Sean Yeo <seansqyeo@gmail.com>
2021-08-27 12:09:53 -0700

    set debug based on environment variables

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,8 @@ LOGIN_URL = 'users:login' # when unauthenticated user tries to view restricted c
 # heroku settings
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
